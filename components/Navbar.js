f177fe4bad4b1ebaaf5a6de97cd8ca1abcb8bff6
Natalia <natalia.marek@protonmail.com>
2020-05-17 00:37:07 +0100

    Initial commit. Created React app, used Next.js for server rendering

---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+
+const Navbar = () => (
+    <div>
+        <ul>
+            <li><Link href="/index" ><a>Home</a></Link></li>
+            <li><Link href="/about" ><a>About</a></Link></li>
+        </ul>
+
+        <style jsx> {`
+            ul {
+                background: #777;
+                color: #fff;
+                list-style: none;
+                display: flex
+            }
+
+            ul li {
+                font-size: 18px;
+                margin-height: 20px;
+                padding: 15px
+            }
+
+            ul li a {
+                color: #ffff;
+                text decoration
+            }
+
+        `}
+        </style>
+    </div>
+);
+
+export default Navbar;
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+
+const About = () => (
+    <div>
+        <Navbar/>
+        <h1>About Bitprice</h1> 
+        <p> App to view bitcoin prices </p>
+    </div>
+);
+
+export default About
